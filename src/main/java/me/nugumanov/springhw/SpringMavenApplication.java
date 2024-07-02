cb91abbac69b1243f9964e64b9c734a214b2eb05
tim40 <tim4054@gmail.com>
2024-07-03 00:05:46 +0300

    HW

--- src/main/java/me/nugumanov/springhw/SpringMavenApplication.java
+++ src/main/java/me/nugumanov/springhw/SpringMavenApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringMavenApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(SpringMavenApplication.class, args);
     }
